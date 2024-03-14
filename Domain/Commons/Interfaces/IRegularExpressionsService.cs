df7361e0b52a272a0fdfa18277b7c219301501a3
Albeiro Marín <xlandru@hotmail.com>
2024-03-14 16:42:14 -0500

    Adds interface and service for validating regular expressions

---
+++ Domain/Commons/Interfaces/IRegularExpressionsService.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Commons.Interfaces;
+
+/// <summary>
+/// Contains Methods for validating strings using regular expressions
+/// </summary>
+public interface IRegularExpressionsService
+{
+    /// <summary>
+    /// Validates a given string to verify if is a valid email Address
+    /// </summary>
+    bool IsValidEmail(string email);
+
+    /// <summary>
+    /// Validates a given string to verify if is a valid name or last name
+    /// </summary>
+    bool IsValidNameOrLastName(string nameType);
+}
---
+++ Domain/Commons/Services/RegularExpressionsService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Commons.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace Domain.Commons.Services;
+
+public sealed class RegularExpressionsService : IRegularExpressionsService
+{
+    public bool IsValidEmail(string email)
+    {
+        // pattern for validating emails
+        string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+        Regex regex = new Regex(pattern);
+        return regex.IsMatch(email);
+    }
+
+    public bool IsValidNameOrLastName(string nameType)
+    {
+        // pattern for validating names ans lastnames
+        string pattern = @"^[a-zA-Z]+(?:['-][a-zA-Z\s*]+)*$";
+        Regex regex = new Regex(pattern);
+        return regex.IsMatch(nameType);
+    }
+}
