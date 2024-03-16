425162cec6fd52f8fa7abb4957b59ab141aa046b
Albeiro Marín <xlandru@hotmail.com>
2024-03-16 09:01:57 -0500

    Creates City entity configuration

---
+++ Infraestructure/Configurations/CityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Domain.Cities;
+
+namespace Infraestructure.Configurations;
+
+/// <summary>
+/// Contains the database table configuration and constraints for the City entity
+/// </summary>
+internal sealed class CityConfiguration : IEntityTypeConfiguration<City>
+{
+    public void Configure(EntityTypeBuilder<City> builder)
+    {
+        builder.ToTable("cities");
+
+        builder.HasKey(city => city.Id);
+
+        builder.Property(city => city.Id);
+
+        builder.Property(city => city.CityName);
+    }
+}
---
+++ Infraestructure/Migrations/20240316135953_FirstMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cities",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    cityName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pK_cities", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cities");
+        }
+    }
+}
