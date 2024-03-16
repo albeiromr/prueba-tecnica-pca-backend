5369aba06d3a5600bc0366a4e929319f990caba1
Albeiro Marín <xlandru@hotmail.com>
2024-03-16 09:05:59 -0500

    Creates configuration for the airlines entity

---
+++ Infraestructure/Configurations/AirlineConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Domain.Cities;
+using Domain.AirLines;
+
+namespace Infraestructure.Configurations;
+
+/// <summary>
+/// Contains the database table configuration and constraints for the City entity
+/// </summary>
+internal sealed class AirlineConfiguration : IEntityTypeConfiguration<Airline>
+{
+    public void Configure(EntityTypeBuilder<Airline> builder)
+    {
+        builder.ToTable("airlines");
+
+        builder.HasKey(airline => airline.Id);
+
+        builder.Property(airline => airline.Id);
+
+        builder.Property(airline => airline.Name);
+
+        builder.Property(airline => airline.Code);
+
+        builder.Property(airline => airline.FlightsCount);
+    }
+}
---
+++ Infraestructure/Migrations/20240316140505_secondMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "airlines",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    code = table.Column<string>(type: "text", nullable: true),
+                    flightsCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pK_airlines", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "airlines");
+        }
+    }
+}
